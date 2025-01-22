4655bab12a0339cdf004e36149b5c8ae2b1783e1
kojesung <js95112345@gmail.com>
2025-01-22 22:12:44 +0900

    feat : 활동 로그 UI

--- src/pages/club-detail/tab/Log.tsx
+++ src/pages/club-detail/tab/Log.tsx
@@ -1,10 +1,38 @@
 import styled from 'styled-components';
 
 export default function Log() {
-    return <LogGrid>활동 로그 이미지들</LogGrid>;
+    return (
+        <Container>
+            <LogGrid>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+                <LogImg></LogImg>
+            </LogGrid>
+        </Container>
+    );
 }
 const LogGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    gap: 16px;
+    gap: 7px;
+`;
+const Container = styled.div`
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+    width: 328px;
+    margin-bottom: 7px;
+`;
+const LogImg = styled.img`
+    width: 95px;
+    height: 95px;
+    border-radius: 5px;
 `;
