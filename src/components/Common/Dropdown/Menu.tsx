fab7951fda3602c0b8420aebfc569d240aa759ae
김예안 <kjan0929@naver.com>
2025-01-26 17:16:07 +0900

    feat: Dropdown 공통 컴포넌트 컴파운드 패턴으로 구현 (#55)

---
+++ src/components/Common/Dropdown/Menu.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+export interface DropdownMenuProps {
+    children: ReactNode;
+    isOpen: boolean;
+}
+
+const DropdownMenu = ({ children, isOpen }: DropdownMenuProps) => {
+    console.log(isOpen);
+
+    return <Container $isOpen={isOpen}>{children}</Container>;
+};
+
+export default DropdownMenu;
+
+const Container = styled.div<{ $isOpen: boolean }>`
+    background-color: ${({ theme }) => theme.colors.white};
+
+    opacity: ${({ $isOpen }) => ($isOpen ? 1 : 0)};
+    transform: ${({ $isOpen }) =>
+        $isOpen ? 'translateY(0)' : 'translateY(-10px)'};
+    transition: opacity 0.3s ease, transform 0.3s ease;
+
+    @keyframes dropdown {
+        0% {
+            transform: translateY(-15px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+    animation: dropdown 0.3s ease;
+`;
