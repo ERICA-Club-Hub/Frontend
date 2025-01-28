5cdd5e51989008ace1df06ccc4f1ecdf25c10a3b
kojesung <js95112345@gmail.com>
2025-01-28 20:45:45 +0900

    feat : 모달 창 box

--- src/pages/club-detail/ActivityLogModal.tsx
+++ src/pages/club-detail/ActivityLogModal.tsx
@@ -1,6 +1,35 @@
+import styled from 'styled-components';
+
 const ActivityLogModal = () => {
     console.log('모달 열림');
-    return <div>모달창!</div>;
+    return (
+        <ModalWrapper>
+            <ModalOverlay>
+                <ModalContent>모달창!</ModalContent>
+            </ModalOverlay>
+        </ModalWrapper>
+    );
 };
+
+const ModalWrapper = styled.div`
+    z-index: 1500;
+    position: absolute;
+`;
+
+const ModalOverlay = styled.div`
+    background-color: rgb(0 0 0 /71%);
+    position: fixed;
+    justify-content: center;
+    inset: 0px;
+    display: flex;
+    align-items: center;
+`;
+
+const ModalContent = styled.div`
+    width: 320px;
+    height: 409px;
+    background-color: white;
+    margin: 0 auto;
+`;
 
 export { ActivityLogModal };
