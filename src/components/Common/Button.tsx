e01df52f7d4897e1b49767057684e078acb1dda8
김예안 <kjan0929@naver.com>
2025-02-11 20:50:36 +0900

    refactor: Button 공통 컴포넌트 varaint prop 추가 및 동적 스타일링 개선 (#26)

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -3,15 +3,19 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 export type ButtonSize = 'small' | 'medium' | 'large';
 
+export type VaraintType = 'filled' | 'outlined';
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     size?: ButtonSize;
+    varaint?: VaraintType;
     isDisabled?: () => boolean;
     handleClick?: () => void;
 }
 
 interface StyledButtonProps {
     size: ButtonSize;
+    varaint: VaraintType;
     disabled: boolean;
 }
 
@@ -45,6 +49,7 @@ const getButtonSize = (size: ButtonSize) => {
  *
  * @param children 컴포넌트 안에 들어갈 내용
  * @param size 버튼 크기(small, large, medium 중 하나)
+ * @param varaint 버튼 스타일 (filled, outlined 중 하나)
  * @param isDisabled 활성화 / 비활성화 시킬 함수(boolean return)
  * @param handleClick onClick시 작동할 이벤트를 담은 함수
  * @returns
@@ -53,6 +58,7 @@ const getButtonSize = (size: ButtonSize) => {
 const Button = ({
     children,
     size = 'medium',
+    varaint = 'filled',
     isDisabled = () => true,
     handleClick,
     ...props
@@ -60,6 +66,7 @@ const Button = ({
     return (
         <StyledButton
             size={size}
+            varaint={varaint}
             disabled={isDisabled()}
             onClick={handleClick}
             {...props}
@@ -75,9 +82,12 @@ const StyledButton = styled.button<StyledButtonProps>`
     justify-content: center;
     border-radius: 10px;
     font-weight: 600;
-    background-color: #33639c;
-    color: white;
-    border: none;
+    background-color: ${({ varaint, theme }) =>
+        varaint === 'outlined' ? theme.colors.white : theme.colors.mainBlue};
+    color: ${({ varaint, theme }) =>
+        varaint === 'outlined' ? theme.colors.mainBlue : theme.colors.white};
+    border: ${({ varaint, theme }) =>
+        varaint === 'outlined' ? `1px solid ${theme.colors.mainBlue}` : 'none'};
     cursor: pointer;
     transition: all 0.2s ease;
     gap: 10px;
@@ -94,7 +104,9 @@ const StyledButton = styled.button<StyledButtonProps>`
     }
 
     &:disabled {
+        border: none;
         background-color: #989898;
+        color: ${({ theme }) => theme.colors.white};
         cursor: not-allowed;
     }
 `;
