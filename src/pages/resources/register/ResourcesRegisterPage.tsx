5d9dcc9ecdca72c93e388859ed1227b3ffc0b0a0
kojesung <js95112345@gmail.com>
2025-02-14 18:18:24 +0900

    style: 자료 등록하기 기본 상태일 때 UI(#70)

--- src/pages/resources/register/ResourcesRegisterPage.tsx
+++ src/pages/resources/register/ResourcesRegisterPage.tsx
@@ -1,3 +1,79 @@
+import Button from '@/components/Common/Button';
+import styled from 'styled-components';
+
 export default function ResourcesRegisterPage() {
-    return <div>d</div>;
+    return (
+        <Container>
+            <CardContainer>
+                <CardContent>
+                    <CardTitle>자료 제목을 입력해 주세요.</CardTitle>
+                    <CardInput placeholder="자료 제목을 정확히 입력해 주세요." />
+                </CardContent>
+                <CardContent>
+                    <CardTitle>자료를 업로드 해주세요.</CardTitle>
+                    <CardUpload placeholder="📂 버튼을 클릭해 업로드 해주세요." />
+                </CardContent>
+            </CardContainer>
+            <Button size="small" variant="filled">
+                저장하기
+            </Button>
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    padding-top: 15px;
+    padding-left: 20px;
+    padding-right: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const CardContent = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    background-color: #ffffff;
+    border-radius: 10px;
+`;
+
+const CardTitle = styled.span`
+    color: black;
+    font-weight: 600;
+    font-size: 14px;
+`;
+
+const CardInput = styled.input`
+    background-color: #f7f7f7;
+    height: 40px;
+    border-radius: 10px;
+    text-align: left;
+    padding-left: 15px;
+    font-size: 14px;
+    font-weight: 400;
+    color: black;
+    &::placeholder {
+        color: #989898;
+    }
+`;
+
+const CardUpload = styled.input`
+    background-color: #f7f7f7;
+    height: 40px;
+    border-radius: 10px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 400;
+    color: black;
+    &::placeholder {
+        color: #989898;
+    }
+`;
