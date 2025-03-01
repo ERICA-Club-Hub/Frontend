9f8b7f400cbba8a5555e916c4c610e50fe8b3e3a
김예안 <kjan0929@naver.com>
2025-03-01 16:15:41 +0900

    feat: 로그인 후 어드민 타입에 따른 리다이렉트 구현 (#106)

--- src/hooks/useAdminLogin.ts
+++ src/hooks/useAdminLogin.ts
@@ -2,11 +2,9 @@ import { apiRequest } from '@/api/apiRequest';
 import { adminType, isAuthenticated } from '@/store/authState';
 import { clubId } from '@/store/clubIdState';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 
 const useAdminLogin = () => {
-    const navigate = useNavigate();
     const [isValidate, setIsValidate] = useState<boolean>(false);
     const setAuthenticated = useSetRecoilState(isAuthenticated);
     const setAdminType = useSetRecoilState(adminType);
@@ -36,18 +34,15 @@ const useAdminLogin = () => {
                 setAuthenticated(true); // 로그인 전역 상태 업데이트
 
                 // 어드민 유형에 따른 전역 상태 설정 및 라우팅
-                if (code === 'A3T78H') {
+                if (code === import.meta.env.VITE_SERVICE_ADMIN_CODE) {
                     // 서비스 관리자
                     setAdminType('service');
-                    navigate('/', { replace: true });
-                } else if (code === 'NM3T78') {
+                } else if (code === import.meta.env.VITE_UNION_ADMIN_CODE) {
                     // 총동연
                     setAdminType('union');
-                    navigate('/admin/union', { replace: true });
                 } else {
                     // 동아리 대표
                     setAdminType('club');
-                    navigate('/admin/club', { replace: true });
                 }
 
                 setClubId(res.result); // clubId 저장
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -64,14 +64,14 @@ export default function AppRoutes() {
             {/* 어드민 접근 권한 필요 -> 권한 없을 때 메인으로 리다이렉트 */}
             <Route path="/admin" element={<AuthGuard />}>
                 {/* 동아리 어드민 */}
-                {/* 총동연, 서비스 관리자, 동아리 대표 모두 접근 가능 */}
-                <Route path="/admin/club">
+                {/* 서비스 관리자, 동아리 대표 접근 가능 */}
+                <Route path="/admin/club/:id">
                     {/* 동아리 어드민 홈*/}
-                    <Route path="/admin/club" element={<AdminClubPage />} />
+                    <Route path="/admin/club/:id" element={<AdminClubPage />} />
 
                     {/* 동아리 활동 로그 */}
                     <Route
-                        path="/admin/club/activity"
+                        path="/admin/club/:id/activity"
                         element={<AdminActivityLogPage />}
                     />
                     {/* 동아리 상세 페이지  */}
@@ -96,13 +96,13 @@ export default function AppRoutes() {
 
                     {/* 동아리 등록 정보 수정 페이지 */}
                     <Route
-                        path="/admin/club/modify"
+                        path="/admin/club/:id/modify"
                         element={<ModifyClubRegisterPage />}
                     />
                 </Route>
 
                 {/* 총동연 어드민 */}
-                {/* 총동연, 서비스 관리자만 접근 가능 */}
+                {/* 총동연, 서비스 관리자 접근 가능 */}
                 <Route path="/admin/union" element={<UnionAdminGuard />}>
                     {/* 총동연 어드민 홈*/}
                     <Route path="/admin/union" element={<AdminUnionPage />} />
