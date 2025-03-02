63a702892994ad54555a648bf93c15c4b0431ebf
김예안 <kjan0929@naver.com>
2025-03-03 04:04:03 +0900

    feat: 삭제 ActionModal 공통 컴포넌트 구현 및 적용 (#30)

--- src/components/UnionNotice/UnionNoticeRegisterForm.tsx
+++ src/components/UnionNotice/UnionNoticeRegisterForm.tsx
@@ -7,11 +7,14 @@ import useUnionQueries from '@/hooks/queries/useUnionQueries';
 import { IUnionNoticeValue } from '@/types';
 import { inputChangeHandler } from '@/utils/inputChangeHandler';
 import { ThumbnailImageUpload } from '@/components/UnionNotice/ThumbnailImageUpload';
+import useToggle from '@/hooks/actions/useToggle';
+import ActionModal from '../Common/Modal/ActionModal';
 
 function UnionNoticeRegisterForm({ mode }: { mode: string }) {
     const navigate = useNavigate();
     const location = useLocation();
     const { announcementId } = location.state || {}; // 전달된 state에서 announcementId 받아오기
+    const { isOpen, toggle } = useToggle();
 
     const [inputValue, setInputValue] = useState({
         title: '',
@@ -90,103 +93,114 @@ function UnionNoticeRegisterForm({ mode }: { mode: string }) {
         postImg !== null;
 
     return (
-        <FormContainer>
-            {/* 공지사항 제목 */}
-            <Wrapper>
-                <Title>공지사항 제목을 입력해 주세요.</Title>
-                <InputField
-                    value={inputValue.title}
-                    name="title"
-                    type="text"
-                    inputSize="medium"
-                    backgroundColor="gray"
-                    placeholder="제목을 입력해 주세요."
-                    disabled={mode === 'edit' && !isEditBtnClicked}
-                    onChange={(e) =>
-                        inputChangeHandler<IUnionNoticeValue>({
-                            e,
-                            setInputValue,
-                        })
-                    }
-                />
-            </Wrapper>
+        <>
+            <FormContainer>
+                {/* 공지사항 제목 */}
+                <Wrapper>
+                    <Title>공지사항 제목을 입력해 주세요.</Title>
+                    <InputField
+                        value={inputValue.title}
+                        name="title"
+                        type="text"
+                        inputSize="medium"
+                        backgroundColor="gray"
+                        placeholder="제목을 입력해 주세요."
+                        disabled={mode === 'edit' && !isEditBtnClicked}
+                        onChange={(e) =>
+                            inputChangeHandler<IUnionNoticeValue>({
+                                e,
+                                setInputValue,
+                            })
+                        }
+                    />
+                </Wrapper>
 
-            {/* 썸네일 업로드 */}
-            <Wrapper>
-                <div className="title-wrapper">
-                    <Title>썸네일을 업로드 해주세요.</Title>
-                    <span>
-                        SNS의 첫 페이지로 들어가는 사진을 업로드해 주세요.
-                    </span>
-                </div>
+                {/* 썸네일 업로드 */}
+                <Wrapper>
+                    <div className="title-wrapper">
+                        <Title>썸네일을 업로드 해주세요.</Title>
+                        <span>
+                            SNS의 첫 페이지로 들어가는 사진을 업로드해 주세요.
+                        </span>
+                    </div>
 
-                <ThumbnailImageUpload
-                    setPostImg={setPostImg}
-                    previewImg={previewImg}
-                    setPreviewImg={setPreviewImg}
-                    mode={mode}
-                    isEditBtnClicked={isEditBtnClicked}
-                />
-            </Wrapper>
+                    <ThumbnailImageUpload
+                        setPostImg={setPostImg}
+                        previewImg={previewImg}
+                        setPreviewImg={setPreviewImg}
+                        mode={mode}
+                        isEditBtnClicked={isEditBtnClicked}
+                    />
+                </Wrapper>
 
-            {/* SNS 링크 입력 */}
-            <Wrapper>
-                <div className="title-wrapper">
-                    <Title>SNS 링크를 입력해 주세요.</Title>
-                    <span>공지사항 클릭 시, 해당 SNS 게시물로 이동합니다.</span>
-                </div>
-                <InputField
-                    value={inputValue.url}
-                    name="url"
-                    type="text"
-                    inputSize="medium"
-                    backgroundColor="gray"
-                    placeholder="SNS 링크를 정확히 입력해 주세요."
-                    disabled={mode === 'edit' && !isEditBtnClicked}
-                    onChange={(e) =>
-                        inputChangeHandler<IUnionNoticeValue>({
-                            e,
-                            setInputValue,
-                        })
-                    }
-                />
-            </Wrapper>
+                {/* SNS 링크 입력 */}
+                <Wrapper>
+                    <div className="title-wrapper">
+                        <Title>SNS 링크를 입력해 주세요.</Title>
+                        <span>
+                            공지사항 클릭 시, 해당 SNS 게시물로 이동합니다.
+                        </span>
+                    </div>
+                    <InputField
+                        value={inputValue.url}
+                        name="url"
+                        type="text"
+                        inputSize="medium"
+                        backgroundColor="gray"
+                        placeholder="SNS 링크를 정확히 입력해 주세요."
+                        disabled={mode === 'edit' && !isEditBtnClicked}
+                        onChange={(e) =>
+                            inputChangeHandler<IUnionNoticeValue>({
+                                e,
+                                setInputValue,
+                            })
+                        }
+                    />
+                </Wrapper>
 
-            <ButtonWrapper>
-                {mode === 'edit' && !isEditBtnClicked ? (
-                    <>
+                <ButtonWrapper>
+                    {mode === 'edit' && !isEditBtnClicked ? (
+                        <>
+                            <Button
+                                type="button"
+                                size="small"
+                                variant="outlined"
+                                outlineColor="#DC5151"
+                                disabled={false}
+                                onClick={toggle}
+                            >
+                                삭제하기
+                            </Button>
+                            <Button
+                                type="button"
+                                size="small"
+                                variant="outlined"
+                                disabled={!isValid}
+                                onClick={() => setIsEditBtnClicked(true)}
+                            >
+                                수정하기
+                            </Button>
+                        </>
+                    ) : (
                         <Button
                             type="button"
                             size="small"
-                            variant="outlined"
-                            outlineColor="#DC5151"
-                            disabled={false}
-                            onClick={handleDeleteUnionNotice}
-                        >
-                            삭제하기
-                        </Button>
-                        <Button
-                            type="button"
-                            size="small"
-                            variant="outlined"
                             disabled={!isValid}
-                            onClick={() => setIsEditBtnClicked(true)}
+                            onClick={handleSaveUnionNotice}
                         >
-                            수정하기
+                            저장하기
                         </Button>
-                    </>
-                ) : (
-                    <Button
-                        type="button"
-                        size="small"
-                        disabled={!isValid}
-                        onClick={handleSaveUnionNotice}
-                    >
-                        저장하기
-                    </Button>
-                )}
-            </ButtonWrapper>
-        </FormContainer>
+                    )}
+                </ButtonWrapper>
+            </FormContainer>
+
+            {/* 삭제하기 Modal */}
+            <ActionModal
+                isOpen={isOpen}
+                toggle={toggle}
+                action={handleDeleteUnionNotice}
+            />
+        </>
     );
 }
 
