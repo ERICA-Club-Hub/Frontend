7b8c39d5b250f595c2d7bbd2fafb7af94e0c778d
kojesung <js95112345@gmail.com>
2025-03-15 23:50:42 +0900

    feat : 미리보기 관련 api 제외 useQuery 작성(#184)

--- src/hooks/queries/club-detail/useClubDetail.ts
+++ src/hooks/queries/club-detail/useClubDetail.ts
@@ -4,35 +4,79 @@ import { useQuery } from '@tanstack/react-query';
 export default function useClubDetail() {
     const useClubDetailSummary = (id: string) => {
         // 동아리 정보 요약
-        return useQuery({
+        const { isPending, isSuccess, data, isError } = useQuery({
             queryKey: ['club', 'summary', id],
             queryFn: async () => {
                 const response = await apiRequest({ url: `/api/clubs/${id}` });
-
                 return response.result;
             },
-            staleTime: 5 * 60 * 1000,
         });
+        return { isPending, isSuccess, data, isError };
     };
-    const useClubSchedule = () => {
+    const useClubSchedule = (id: string) => {
         // 동아리 연간 일정
-        const { isPending, isSuccess, data, isError } = useQuery({});
+        const { isPending, isSuccess, data, isError } = useQuery({
+            queryKey: ['club', 'schedule', id],
+            queryFn: async () => {
+                const response = await apiRequest({
+                    url: `/api/clubs/${id}/schedules`,
+                });
+                return response.result;
+            },
+        });
+        return { isPending, isSuccess, data, isError };
     };
-    const useClubIntroduction = () => {
+    const useClubIntroduction = (id: string) => {
         // 동아리 소개
-        const { isPending, isSuccess, data, isError } = useQuery({});
+        const { isPending, isSuccess, data, isError } = useQuery({
+            queryKey: ['club', 'intro', id],
+            queryFn: async () => {
+                const response = await apiRequest({
+                    url: `/api/clubs/${id}/introduction`,
+                });
+                return response.result;
+            },
+        });
+        return { isPending, isSuccess, data, isError };
     };
-    const useClubRecruitment = () => {
+    const useClubRecruitment = (id: string) => {
         // 동아리 모집 안내
-        const { isPending, isSuccess, data, isError } = useQuery({});
+        const { isPending, isSuccess, data, isError } = useQuery({
+            queryKey: ['club', 'intro', id],
+            queryFn: async () => {
+                const response = await apiRequest({
+                    url: `/api/clubs/${id}/recruitment`,
+                });
+                return response.result;
+            },
+        });
+        return { isPending, isSuccess, data, isError };
     };
-    const useClubActivityLog = () => {
+    const useClubActivityLog = (id: string) => {
         // 동아리 활동 로그
-        const { isPending, isSuccess, data, isError } = useQuery({});
+        const { isPending, isSuccess, data, isError } = useQuery({
+            queryKey: ['club', 'activities', id],
+            queryFn: async () => {
+                const response = await apiRequest({
+                    url: `/api/activities/club/${id}`,
+                });
+                return response.result;
+            },
+        });
+        return { isPending, isSuccess, data, isError };
     };
-    const useClubActivity = () => {
-        // 활동 로그 하나
-        const { isPending, isSuccess, data, isError } = useQuery({});
+    const useClubActivity = (activityId: string) => {
+        // 활동 로그 상세
+        const { isPending, isSuccess, data, isError } = useQuery({
+            queryKey: ['club', 'activity', activityId],
+            queryFn: async () => {
+                const response = await apiRequest({
+                    url: `/api/activities/${activityId}`,
+                });
+                return response.result;
+            },
+        });
+        return { isPending, isSuccess, data, isError };
     };
     return {
         useClubDetailSummary,
