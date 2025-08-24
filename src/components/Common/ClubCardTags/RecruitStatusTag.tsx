90e73eab550ab0903ede18371686811c4ff1cf4e
kojesung <js95112345@gmail.com>
2025-08-24 18:18:39 +0900

    feat: 동아리 모집 상태 태그 컴포넌트 제작 및 모집 상태 관련 유틸 수정

--- src/components/Common/ClubCardTags/RecruitStatusTag.tsx
+++ src/components/Common/ClubCardTags/RecruitStatusTag.tsx
@@ -1,3 +1,24 @@
-export default function RecruitStatusTag() {
-    return <div></div>;
+import {
+    RecruitmentStatus,
+    getRecruitmentStatusInfo,
+} from '@/utils/clubDetail/getRecruitmentStatus';
+import ClubTag from '../ClubTag';
+
+interface RecruitStatusTagProps {
+    recruitmentStatus: RecruitmentStatus;
+}
+
+export default function RecruitStatusTag({
+    recruitmentStatus,
+}: RecruitStatusTagProps) {
+    const { label, backgroundColor, textColor } =
+        getRecruitmentStatusInfo(recruitmentStatus);
+
+    return (
+        <ClubTag
+            text={label}
+            backgroundColor={backgroundColor}
+            textColor={textColor}
+        />
+    );
 }
