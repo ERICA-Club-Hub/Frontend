9e0bb0ea8bc5e61904097003f5662b2b92ba8b0d
kojesung <js95112345@gmail.com>
2025-08-24 18:06:02 +0900

    feat: 동아리 유형별 태그 컴포넌트

--- src/components/Common/ClubCardTags/ClubTypeTag.tsx
+++ src/components/Common/ClubCardTags/ClubTypeTag.tsx
@@ -1,3 +1,21 @@
-export default function ClubTypeTag() {
-    return <div></div>;
+import {
+    Category,
+    getCategoryEmoji,
+    getCategoryMapping,
+} from '@/utils/clubDetail/getCategoryEmoji';
+import ClubTag from '../ClubTag';
+
+interface ClubTypeTagProps {
+    clubCategory: Category;
+}
+
+export default function ClubTypeTag({ clubCategory }: ClubTypeTagProps) {
+    return (
+        <ClubTag
+            text={getCategoryMapping(clubCategory)}
+            emoji={getCategoryEmoji(clubCategory)}
+            backgroundColor="rgba(238, 244, 255, 1)"
+            textColor="rgba(51, 99, 156, 1)"
+        />
+    );
 }
