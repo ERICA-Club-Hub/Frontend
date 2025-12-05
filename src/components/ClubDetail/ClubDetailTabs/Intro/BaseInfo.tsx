8ce0f84326f3ce1d79115a686a99d781e2950b89
kojesung <js95112345@gmail.com>
2025-12-05 15:14:09 +0900

    feat: 상세 페이지 인스타 클릭 시 인스타그램 프로필로 이동

--- src/components/ClubDetail/ClubDetailTabs/Intro/BaseInfo.tsx
+++ src/components/ClubDetail/ClubDetailTabs/Intro/BaseInfo.tsx
@@ -13,24 +13,35 @@ import {
 export default function BaseInfo() {
     const { id, isPreview } = useIsPreview();
     const { data } = useClubInfo(id || '', isPreview);
+
+    const handleSnsClick = () => {
+        if (data?.snsUrl) {
+            const username = data.snsUrl.replace('@', '');
+            window.open(`https://www.instagram.com/${username}`, '_blank');
+        }
+    };
+
     const baseInfo = [
         {
             key: 'leader',
             iconUrl: jjang,
             label: '대표',
             value: data?.leaderName || '대표자 이름이 없습니다.',
+            clickable: false,
         },
         {
             key: 'contact',
             iconUrl: phone,
             label: '연락처',
             value: data?.leaderPhone || '연락처 정보가 제공되지 않았습니다.',
+            clickable: false,
         },
         {
             key: 'meeting',
             iconUrl: label,
             label: '정기모임',
             value: data?.activities || '정해진 정기모임이 없습니다.',
+            clickable: false,
         },
         {
             key: 'fee',
@@ -39,22 +50,31 @@ export default function BaseInfo() {
             value: data?.membershipFee
                 ? `${data.membershipFee}원`
                 : '회비 정보가 제공되지 않았습니다.',
+            clickable: false,
         },
         {
             key: 'sns',
             iconUrl: sns,
             label: 'SNS',
             value: data?.snsUrl ? `@${data.snsUrl}` : 'SNS 정보가 없습니다.',
+            clickable: !!data?.snsUrl,
+            onClick: handleSnsClick,
         },
     ];
+
     return (
         <ContentBlock title="동아리 기본 정보">
             <ClubDetails>
                 {baseInfo.map((info) => (
-                    <DetailRow>
+                    <DetailRow key={info.key}>
                         <IconImage src={info.iconUrl} alt={info.label} />
                         <DetailLabel>{info.label}</DetailLabel>
-                        <DetailValue>{info.value}</DetailValue>
+                        <DetailValue
+                            $clickable={info.clickable}
+                            onClick={info.clickable ? info.onClick : undefined}
+                        >
+                            {info.value}
+                        </DetailValue>
                     </DetailRow>
                 ))}
             </ClubDetails>
@@ -84,6 +104,8 @@ const DetailLabel = styled.span`
     min-width: 80px;
 `;
 
-const DetailValue = styled.span`
+const DetailValue = styled.span<{ $clickable?: boolean }>`
     color: #333;
+    cursor: ${({ $clickable }) => ($clickable ? 'pointer' : 'default')};
+    text-decoration: ${({ $clickable }) => ($clickable ? 'underline' : 'none')};
 `;
