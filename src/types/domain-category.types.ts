76f95f9d01c6a1c4bfa74ac6e18152b9bfdad9d8
kojesung <js95112345@gmail.com>
2026-01-16 14:19:47 +0900

    feat: 중앙동아리, 정렬 기준 타입 생성

--- src/types/domain-category.types.ts
+++ src/types/domain-category.types.ts
@@ -3,12 +3,21 @@ import { Category } from '@/api/data-contracts';
 /**
  * 학과 type
  */
-export type department = NonNullable<Category['central']>;
+export type DepartmentCode = NonNullable<Category['department']>;
 /**
  * 단과대 type
  */
 export type CollegeCode = NonNullable<Category['college']>;
 /**
- * 중앙동아리 분과 type
+ * 연합동아리 분과 type
  */
 export type UnionCategoryCode = NonNullable<Category['union']>;
+/**
+ * 중앙동아리 분과 type
+ */
+export type CentralCategoryCode = NonNullable<Category['central']>;
+
+/**
+ * 정렬 기준 type
+ */
+export type SortByCode = 'NAME_ASC' | 'CATEGORY_ASC' | 'RECRUITMENT_STATUS_ASC';
