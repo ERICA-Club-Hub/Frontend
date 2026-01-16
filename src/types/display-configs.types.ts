3c8c8fade43bb1e8072216a011d72723afa454a6
kojesung <js95112345@gmail.com>
2026-01-16 14:20:41 +0900

    feat: 각 타입이 다루는 필드들(한국어, 이모지, 배경색 등) 매핑할 수 있는 Config 타입 생성

---
+++ src/types/display-configs.types.ts
@@ -0,0 +1,55 @@
+import {
+    CentralCategoryCode,
+    DepartmentCode,
+} from '@/utils/search/searchKeywordMapping';
+import { CollegeCode, UnionCategoryCode } from './domain-category.types';
+import { RecruitmentStatus } from './recruitment-status.type';
+
+/**
+ * 단과대 라벨과 이모지
+ */
+export interface CollegeDisplayConfig {
+    code: CollegeCode;
+    label: string;
+    emoji: string;
+}
+
+/**
+ * 중앙동아리 분과 라벨과 이모지
+ */
+export interface CentralCategoryDisplayConfig {
+    code: CentralCategoryCode;
+    label: string;
+    emoji: string;
+}
+
+/**
+ * 학과별 라벨과 이모지
+ */
+export interface DepartmentDisplayConfig {
+    code: DepartmentCode;
+    label: string;
+}
+
+/**
+ * 연합동아리 분과 라벨과 이모지
+ */
+export interface UnionCategoryDisplayConfig {
+    code: UnionCategoryCode;
+    label: string;
+}
+
+/**
+ * 리크루팅 상태별 이모지와 글자 및 배경 색
+ */
+export interface RecruitmentStatusDisplayConfig {
+    code: RecruitmentStatus;
+    label: string;
+    textColor: string;
+    backgroundColor: string;
+}
+
+export interface SortByDisplayConfig {
+    code: string;
+    label: string;
+}
