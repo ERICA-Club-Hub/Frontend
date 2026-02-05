4b6d8220b150a349802c77fd580357b0d2785bc5
김예안 <kjan0929@naver.com>
2026-02-05 13:42:55 +0900

    refactor: textarea v2 디자인 시스템 적용 및 개선 (#243)

--- src/components/InputField/TextArea.tsx
+++ src/components/InputField/TextArea.tsx
@@ -1,59 +1,24 @@
-import { cva } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
+import { ComponentProps, forwardRef } from 'react';
 
-type Size = 'small' | 'medium' | 'large';
-type BackgroundColor = 'white' | 'gray';
+type TextAreaProps = ComponentProps<'textarea'>;
 
-interface TextAreaProps
-    extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-    size: Size;
-    backgroundColor?: BackgroundColor;
-}
-
-const textAreaVariants = cva(
-    'text-caption font-medium leading-[18px] text-[#232323] resize-none',
-    {
-        variants: {
-            size: {
-                small: 'w-[230px] h-[120px] p-[10px] rounded-[5px]',
-                medium: 'w-[320px] h-[100px] px-[17px] py-[14px] rounded-[10px]',
-                large: 'w-[280px] h-[170px] p-[15px] rounded-[10px]',
-            },
-            backgroundColor: {
-                white: 'bg-white',
-                gray: 'bg-neutral-100',
-            },
-        },
-        defaultVariants: {
-            size: 'large',
-            backgroundColor: 'white',
-        },
-    }
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+    ({ className, ...props }, ref) => {
+        return (
+            <textarea
+                ref={ref}
+                className={cn(
+                    'w-[320px] min-h-[45px] p-[12px] rounded-[8px] border-[0.6px] border-solid border-neutral-100 text-b4 text-text-main bg-neutral-100 transition-all duration-300 ease-in',
+                    'field-sizing-content resize-none',
+                    'placeholder:text-b4 placeholder:text-neutral-400 outline-none focus:bg-neutral-00 focus:border-[0.6px] focus:border-solid focus:border-neutral-150',
+                    '[&:not(:placeholder-shown)]:bg-neutral-00 [&:not(:placeholder-shown)]:border-neutral-150',
+                    className,
+                )}
+                {...props}
+            />
+        );
+    },
 );
 
-/**
- * TextArea 컴포넌트는 사용자 정의 가능한 크기와 배경색을 가진 스타일된 텍스트 영역을 렌더링합니다.
- *
- * @param {Size} [size = 'large'] - 텍스트 영역의 크기. 'small', 'medium', 'large' 중 하나 입력 가능
- * @param {BackgroundColor} [backgroundColor='white'] - 텍스트 영역의 배경색. 'white' 또는 'gray' 중 하나
- * @param {React.TextareaHTMLAttributes<HTMLTextAreaElement>} props - 텍스트 영역 요소에 전달할 추가 속성
- *
- * @returns {JSX.Element} 스타일된 텍스트 영역 컴포넌트
- */
-const TextArea = ({
-    size = 'large',
-    backgroundColor = 'white',
-    ...props
-}: TextAreaProps) => {
-    return (
-        <textarea
-            className={cn(
-                textAreaVariants({ size, backgroundColor }),
-                'placeholder:text-body-03 placeholder:font-normal placeholder:text-neutral-400'
-            )}
-            {...props}
-        />
-    );
-};
-
-export { TextArea };
+export default TextArea;
