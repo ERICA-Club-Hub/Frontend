916d9208f93f03147d1ee520660018ee17038d66
김예안 <kjan0929@naver.com>
2026-02-07 00:35:59 +0900

    feat: 프로필 필드 스키마 정의 (#246)

---
+++ src/domains/club/profile/model/profile.schema.ts
@@ -0,0 +1,44 @@
+import {
+    CentralCategoryCode,
+    ClubType,
+    CollegeCode,
+    DepartmentCode,
+    UnionCategoryCode,
+} from '@/types/category.types';
+import { z } from 'zod';
+
+const categorySchema = z.object({
+    central: z
+        .string()
+        .nullable()
+        .optional() as z.ZodType<CentralCategoryCode | null>,
+    union: z
+        .string()
+        .nullable()
+        .optional() as z.ZodType<UnionCategoryCode | null>,
+    college: z.string().nullable().optional() as z.ZodType<CollegeCode | null>,
+    department: z
+        .string()
+        .nullable()
+        .optional() as z.ZodType<DepartmentCode | null>,
+});
+
+const profileSchema = z.object({
+    clubName: z.string().min(1),
+    clubType: z.string().min(1) as z.ZodType<ClubType>,
+    category: categorySchema,
+    image: z.string().optional(),
+    oneLiner: z.string().min(1).max(18),
+});
+
+const registrationSchema = profileSchema.extend({
+    leaderEmail: z.email(), // 유저명 + @ + 도메인 + . + 최상위도메인
+    briefIntroduction: z.string().optional(),
+});
+
+type RegistrationSchema = z.infer<typeof registrationSchema>;
+type ProfileSchema = z.infer<typeof profileSchema>;
+type FormValues = RegistrationSchema | ProfileSchema;
+
+export { registrationSchema, profileSchema };
+export type { FormValues };
