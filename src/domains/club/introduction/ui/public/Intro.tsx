414bca0c878eb06cd241d0c1f27c816ac0c9f016
kojesung <js95112345@gmail.com>
2026-02-06 15:57:43 +0900

    feat: 동아리 상세 페이지 동아리 소개 탭 구현

---
+++ src/domains/club/introduction/ui/public/Intro.tsx
@@ -0,0 +1,32 @@
+import {
+    useClubApi,
+    useIsPreview,
+} from '@/domains/club/introduction/api/useClubIntro';
+import { ClubDetailResponse } from '@/api/data-contracts';
+import ClubDetailDefaultInfo from '@/domains/club/introduction/ui/public/ClubDetailDefaultInfo';
+import ClubDetailCard from '@/domains/shared/components/layout/ClubDetailCard';
+import ClubDetailText from '@/domains/shared/components/club-detail-info/ClubDetailText';
+
+export default function Intro() {
+    const { id, isPreview } = useIsPreview();
+
+    // TODO isLoading 전달
+    const { data } = useClubApi<ClubDetailResponse>({
+        clubId: id,
+        isPreview,
+        errorMessage: '정보 조회 실패',
+    });
+
+    return (
+        <section className="flex flex-col gap-2.5">
+            <ClubDetailCard title="동아리 기본 정보">
+                {/* TODO isLoading 전달 */}
+                <ClubDetailDefaultInfo data={data} />
+            </ClubDetailCard>
+
+            <ClubDetailCard title="동아리 소개">
+                <ClubDetailText text={data?.description} />
+            </ClubDetailCard>
+        </section>
+    );
+}
