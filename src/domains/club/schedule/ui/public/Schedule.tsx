d24292f71201e051a26063285c1cfb766c5828c2
kojesung <js95112345@gmail.com>
2026-02-06 16:03:09 +0900

    feat: 동아리 상세 연간일정, 모집 정보 탭 구현

---
+++ src/domains/club/schedule/ui/public/Schedule.tsx
@@ -0,0 +1,22 @@
+import {
+    useClubSchedules,
+    useIsPreview,
+} from '@/domains/club/introduction/api/useClubIntro';
+import ClubDetailSchedule from './ClubDetailSchedule';
+import ClubDetailText from '@/domains/shared/components/club-detail-info/ClubDetailText';
+import ClubDetailCard from '@/domains/shared/components/layout/ClubDetailCard';
+
+export default function Schedule() {
+    const { id, isPreview } = useIsPreview();
+    const { data: scheduleData } = useClubSchedules({ clubId: id, isPreview });
+    return (
+        <section className="flex flex-col gap-2.5">
+            <ClubDetailCard title="주요 연간 일정">
+                <ClubDetailSchedule data={scheduleData} />
+            </ClubDetailCard>
+            <ClubDetailCard title="활동 안내">
+                <ClubDetailText text={scheduleData?.scheduleDescription} />
+            </ClubDetailCard>
+        </section>
+    );
+}
