fdb36fc7f028cba01fce12db7158c76b4b7efaa4
김예안 <kjan0929@naver.com>
2026-02-07 21:57:45 +0900

    refactor: 로그인 validate 로직 개선 (#246)
    오류 발생 후 입력 시작 시 오류 관련 스타일 제거

--- src/pages/admin/auth/AdminLoginPage.tsx
+++ src/pages/admin/auth/AdminLoginPage.tsx
@@ -8,10 +8,11 @@ import { PATHS } from '@/routes/paths';
 
 const AdminLoginPage = () => {
     const [code, setCode] = useState<string>('');
-    const { isValidate, handleLogin } = useAdminLogin();
+    const { isValidate, setIsValidate, handleLogin } = useAdminLogin();
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setCode(e.target.value);
+        setIsValidate(true);
     };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -40,7 +41,7 @@ const AdminLoginPage = () => {
                     <Button type="submit" size="lg">
                         어드민 로그인하기
                     </Button>
-                    {!code && (
+                    {!isValidate && (
                         <p
                             className={cn(
                                 'absolute top-[112px] left-[15px] text-body-03 font-medium text-sub-warning transition-opacity duration-500 ease-in-out',
