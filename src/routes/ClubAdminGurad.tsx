3a541bc5924d33f29c8b2bd82afdf0c5ad94101d
김예안 <kjan0929@naver.com>
2026-02-08 21:44:40 +0900

    refactor: 동아리 어드민 가드 예외 처리 개선
    clubId에 따른 예외 처리 로직 추가

--- src/routes/ClubAdminGurad.tsx
+++ src/routes/ClubAdminGurad.tsx
@@ -1,13 +1,29 @@
 import { adminTypeSelector } from '@/domains/auth/model/auth.atom';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
+import { PATHS } from './paths';
+import { clubIdSelector } from '@/domains/auth/model/clubInfo.atom';
 
 export default function ClubAdminGurad() {
-    const adminType = useRecoilValue(adminTypeSelector);
+    const { id } = useParams<{ id: string }>();
 
-    return adminType === 'club' || adminType === 'service' ? (
+    const adminType = useRecoilValue(adminTypeSelector);
+    const myClubId = useRecoilValue(clubIdSelector);
+
+    const clubId = Number(id);
+    const isValidClubId = id && !isNaN(clubId) && clubId > 0;
+
+    // 서비스 관리자일 경우 모든 동아리 접근 허용
+    if (adminType === 'service') return <Outlet />;
+
+    if (!isValidClubId) return <Navigate to={PATHS.WRONG_ACCESS} replace />;
+
+    if (myClubId !== clubId)
+        return <Navigate to={PATHS.WRONG_ACCESS} replace />;
+
+    return adminType && adminType === 'club' ? (
         <Outlet />
     ) : (
-        <Navigate to="/admin/login" />
+        <Navigate to={PATHS.ADMIN_LOGIN} />
     );
 }
