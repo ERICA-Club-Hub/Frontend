332f4ec07854b5274345cb78b413aa0016447642
김예안 <kjan0929@naver.com>
2026-02-08 22:25:24 +0900

    refactor: 동아리 어드민 분기 처리 로직 개선

--- src/routes/ClubAdminGurad.tsx
+++ src/routes/ClubAdminGurad.tsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet, useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { PATHS } from './paths';
 import { clubIdSelector } from '@/domains/auth/model/clubInfo.atom';
+import { ADMIN_TYPE } from '@/types/admin.types';
 
 export default function ClubAdminGurad() {
     const { id } = useParams<{ id: string }>();
@@ -14,16 +15,17 @@ export default function ClubAdminGurad() {
     const isValidClubId = id && !isNaN(clubId) && clubId > 0;
 
     // 서비스 관리자일 경우 모든 동아리 접근 허용
-    if (adminType === 'service') return <Outlet />;
+    if (adminType === ADMIN_TYPE.SERVICE) return <Outlet />;
 
     if (!isValidClubId) return <Navigate to={PATHS.WRONG_ACCESS} replace />;
 
-    if (myClubId !== clubId)
-        return <Navigate to={PATHS.WRONG_ACCESS} replace />;
+    if (adminType === ADMIN_TYPE.CLUB) {
+        if (myClubId !== clubId) {
+            return <Navigate to={PATHS.WRONG_ACCESS} replace />;
+        }
 
-    return adminType && adminType === 'club' ? (
-        <Outlet />
-    ) : (
-        <Navigate to={PATHS.ADMIN_LOGIN} />
-    );
+        return <Outlet />;
+    }
+
+    return <Navigate to={PATHS.ADMIN_LOGIN} />;
 }
