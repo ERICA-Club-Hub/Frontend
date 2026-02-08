42174c5c419d34e57356fea91d15ec4a54cc23af
김예안 <kjan0929@naver.com>
2026-02-08 21:43:23 +0900

    refactor: 공용 유틸함수 파라미터 추가
    params 파라미터 추가

--- src/types/api.types.ts
+++ src/types/api.types.ts
@@ -9,6 +9,7 @@ interface RequestConfig {
     url: string;
     method?: string; // default GET으로 할거라 일단 optional
     data?: unknown;
+    params?: unknown;
     headers?: Record<string, string>;
     requireToken?: boolean;
     clubName?: string | undefined;
