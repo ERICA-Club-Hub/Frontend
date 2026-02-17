8ec9b6e27457816673217bda4919467c30576499
김예안 <kjan0929@naver.com>
2026-02-17 18:14:28 +0900

    refactor: 폼데이터 유틸함수 도메인 공용 디렉토리로 분리 (#273)

--- src/domains/club/profile/api/profile.mutations.ts
+++ src/domains/club/profile/api/profile.mutations.ts
@@ -4,59 +4,9 @@ import { MAX_FILE_SIZE_ERROR_MESSAGE } from '@/constants/max-file-size.constant'
 import { useErrorHandler } from '@/hooks/useErrorHandler';
 import { validateFileSize } from '@/utils/fileValidator';
 import { useMutation } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 import { FormValues } from '../model/profile.schema';
-import { createFormData } from './createFormData';
-import { PATHS } from '@/routes/paths';
 import { queryClient } from '@/api/queryClient';
-
-/**
- * 동아리 등록 요청
- */
-export const useClubRegisterMutation = () => {
-    const navigate = useNavigate();
-    const { handleError } = useErrorHandler();
-    const { showToast } = useToast();
-
-    return useMutation({
-        mutationFn: async ({
-            data,
-            postImg,
-        }: {
-            data: FormValues;
-            postImg: File | File[] | null;
-        }) => {
-            const formData = createFormData(data, postImg);
-
-            validateFileSize(formData);
-
-            return await apiRequest({
-                url: `/api/clubs/registrations`,
-                method: 'POST',
-                data: formData,
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-                requireToken: true,
-            });
-        },
-        onSuccess: () => {
-            navigate(PATHS.CLUB_REGISTRATION_COMPLETED, {
-                replace: true,
-            });
-        },
-        onError: (error) => {
-            if (
-                error instanceof Error &&
-                error.message === MAX_FILE_SIZE_ERROR_MESSAGE
-            ) {
-                showToast(error.message);
-            } else {
-                handleError(error);
-            }
-        },
-    });
-};
+import { createFormData } from '@/utils/createFormData';
 
 /**
  * 동아리 기본 정보 수정 요청
