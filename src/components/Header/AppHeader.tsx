51ecbd78de2cf159bd69b269e610bbea8fb7b7c1
김예안 <kjan0929@naver.com>
2026-02-23 23:42:32 +0900

    feat: AppHeader 뒤로가기 버튼 추가 (#290)

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -1,20 +1,32 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import HeaderDropdownMenu from './HeaderDropdownMenu';
 import Logo from '@/assets/logo/header-logo.svg?react';
 import LogoText from '@/assets/common/hanjari.svg?react';
 import HomeIcon from '@/assets/common/home.svg?react';
 import { PATHS } from '@/routes/paths';
+import NavigateArrow from '@/assets/common/navigate-arrow.svg?react';
 
 function AppHeader() {
     const location = useLocation();
+    const navigate = useNavigate();
     const isHomePage = location.pathname === PATHS.HOME;
 
     return (
         <header className="fixed top-0 left-1/2 -translate-x-1/2 flex justify-between items-center w-full min-w-[360px] max-w-[600px] min-h-[56px] px-[20px] bg-white cursor-pointer z-[100]">
-            <Link to="/" className="flex items-center gap-[4px]">
-                <Logo />
-                <LogoText />
-            </Link>
+            {isHomePage ? (
+                <Link to="/" className="flex items-center gap-[4px]">
+                    <Logo />
+                    <LogoText />
+                </Link>
+            ) : (
+                <button
+                    onClick={() => navigate(-1)}
+                    className="transform rotate-180 cursor-pointer"
+                    aria-label="이전 페이지로 이도"
+                >
+                    <NavigateArrow width="28" height="28" />
+                </button>
+            )}
 
             {/* 드롭다운 매뉴 */}
             <div className="flex items-center gap-[4px]">
