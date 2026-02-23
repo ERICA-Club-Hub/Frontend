3e92a53611040a4830e13b53d1eb9624a2e56c06
kojesung <js95112345@gmail.com>
2026-02-23 23:02:05 +0900

    feat: button에 onClick으로 window scroll 조절하던 방식 -> useEffect로 매 페이지 스크롤 변경하는 컴포넌트 추가

--- src/components/Layout/RootLayout.tsx
+++ src/components/Layout/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from 'react';
 import { AppHeader } from '../Header/AppHeader';
+import ScrollToTop from '@/routes/ScrollTop';
 
 function RootLayout({ children }: { children: ReactNode }) {
     return (
         <main className="min-w-[360px] max-w-[600px] min-h-screen pt-[55px] mx-auto bg-background-default">
+            <ScrollToTop />
             <AppHeader />
             {children}
         </main>
