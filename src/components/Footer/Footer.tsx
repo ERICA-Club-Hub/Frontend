419619a60d2f49a781a70d651aadda0a31e2af37
kojesung <js95112345@gmail.com>
2026-02-24 23:02:30 +0900

    fix: 배너 및 푸터 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <div className="text-[#989898] text-center text-caption font-medium mt-[20px] mb-[60px]">
-            ⓒ 2025. 한자리. All Rights Reserved.
+            ⓒ 2026. 한자리. All Rights Reserved.
         </div>
     );
 };
